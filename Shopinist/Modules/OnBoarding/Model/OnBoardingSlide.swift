972dd080e4485fa1409a7ebbfb258c2d9caac891
Mohammad Amr <mohammadamr@Mohammads-Mac.local>
2022-06-24 16:57:44 +0200

    Feat(UI): Added launch and onBoarding Screens

---
+++ Shopinist/Modules/OnBoarding/Model/OnBoardingSlide.swift
@@ -0,0 +1,15 @@
+//
+//  OnBoardingSlide.swift
+//  Shopinist
+//
+//  Created by Mohammad Amr on 6/24/22.
+//  Copyright © 2022 MAD 42. All rights reserved.
+//
+
+import Foundation
+
+struct OnBoardingSlide {
+    let title : String
+    let desc : String
+    let animation : String
+}
